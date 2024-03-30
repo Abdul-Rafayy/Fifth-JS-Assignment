9dab12ceae618d6c9d082c3ef33713cf26aaf512
Abdul-Rafayy <155812890+Abdul-Rafayy@users.noreply.github.com>
2024-03-30 20:34:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+// Q 01
+// var array = [[[]]]
+
+// Q 02
+// var matrix = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+// ]
+// console.log(matrix);
+
+// Q 03
+// for (var i = 1; i <=10; i++) {
+//     document.write(i+"<br>")
+    
+// }
+
+// Q 04
+// var number = +prompt("Enter a number to show its Multiplication Table.")
+// var length = +prompt("Enter length of Multiplication Table.")
+
+// for (var i = 1; i<=length; i++) {
+//     document.write(number + " x " + i + " = " + number*i + "<br>")
+    
+// }
+
+// Q 05
+// var fruits = ["apple", "banana", "mango", "orange","strawberry"]
+// for (var i = 0; i<fruits.length; i++) {
+//     document.write(fruits[i] + "<br>")
+//     document.write("Element at index " + i + " is " + fruits[i] +"<br>");
+// }
+
+// Q 06
+// document.write(`<h3>Counting:<br>`)
+// for (var i = 1; i <=20; i++) {
+    
+//     document.write(i + ",")
+    
+// }
+// document.write(`<h3>Reverse Counting:<br>`)
+// for (var i = 20; i >=1; i--) {
+    
+//     document.write(i + ",")
+    
+// }
+// document.write(`<h3>Even:<br>`)
+// for (var i = 0; i <=20; i+=2) {
+    
+//     document.write(i + ",")
+    
+// }
+// document.write(`<h3>Odd:<br>`)
+// for (var i = 1; i <=20; i+=2) {
+    
+//     document.write(i + ",")
+    
+// }
+// document.write(`<h3>Series:<br>`)
+// for (var i = 2; i <=20; i+=2) {
+    
+//     document.write(i +"k"+ ",")
+    
+// }
+
+// Q 07
+// var userInput = prompt("Welcome to Ideal Bakery,What do you want to order sir/ma'am?")
+// var bakeryItems = ["cake", "apple pie", "cookie", "chips", "patties"]
+// var match = "no"
+// for (let i = 0; i < bakeryItems.length; i++) {
+//     if (userInput===bakeryItems[i]) { 
+//     match = "yes"
+//         alert(userInput + " is available at index " + i +" in our bakery" )
+//     break     
+//     }  
+// }
+// if(match=="no"){
+//     alert("We are sorry." + userInput + " is not available in our bakery")
+// }
+
+
+// Q 08
+// var number  = [24, 53, 78, 91, 12];
+// var largest = Math.max(...number);
+
+// document.write(`The largest number in the array is: ${largest}`);
+
+
+// Q 09
+// var number  = [24, 53, 78, 91, 12];
+// var smallest = Math.min(...number);
+
+// document.write(`The smallest number in the array is: ${smallest}`);
+
+
+// Q 10
+// for (let i = 5; i <=100; i+=5) {
+//     document.write(i + ",")
+    
+    
+// }
